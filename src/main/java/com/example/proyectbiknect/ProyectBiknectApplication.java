958fedef8e0c9998e0e75a3c24ac1a156e3b990d
Mija9000 <mijail.saltsin@utec.edu.pe>
2025-04-28 23:56:41 -0500

    primeros archivos(faltan)

---
+++ src/main/java/com/example/proyectbiknect/ProyectBiknectApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectbiknect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectBiknectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectBiknectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyectbiknect/ProyectBiknectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectbiknect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectBiknectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
